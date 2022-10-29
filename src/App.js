0c389866550856cd89416edb6d148cab88829d9e
unknown <chegoun2019@gmail.com>
2022-10-29 20:00:58 +0200

    create route and pages and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/profile' element={<Profile/>} />
+       <Route path='/sign-in' element={<SignIn/>} />
+       <Route path='/sign-up' element={<SignUp/>} />
+       <Route path='/offers' element={<Offers/>} />
+       <Route path='/forgot-password' element={<ForgotPassword />} />
+    
+      </Routes>
+    </BrowserRouter>
+   
   );
 }
 
